0a2cfaa03a1b519749bb95c0acf360639cd93243
Ryan <rmurph1800@gmail.com>
2021-09-28 19:13:24 -0400

    finally got it

--- Desktop/projects/jest-test/__tests__/Player.test.js
+++ Desktop/projects/jest-test/__tests__/Player.test.js
@@ -14,7 +14,7 @@ test("creates a player object", () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
 
-    // expect(player.inventory).toEqual(
-    //     expect.arrayContaining([expect.any(Object)])
-    // );
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
